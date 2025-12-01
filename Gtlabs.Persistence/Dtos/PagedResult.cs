c6bb0a1a03d85936d8c461d153338fc2b0b0a965
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-11-30 23:15:45 -0300

    feat: added queryable extension

---
+++ Gtlabs.Persistence/Dtos/PagedResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Gtlabs.Persistence.Dtos;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; set; } = default!;
+    public int TotalCount { get; set; }
+}
---
+++ Gtlabs.Persistence/Extensions/QueryableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Gtlabs.Persistence.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> Page<T>(this IQueryable<T> query, int page, int pageSize)
+        => query.Skip((page - 1) * pageSize).Take(pageSize);
+}
