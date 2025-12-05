6d843473bfc53bf8eb7a977cfa1bcf772e98367c
Matheus Guedes Bertaiolli <70043048+matheusgbrt@users.noreply.github.com>
2025-12-05 20:56:53 -0300

    fix: DI

--- Gtlabs.AspNet/Extensions/InitializeService.cs
+++ Gtlabs.AspNet/Extensions/InitializeService.cs
@@ -1,4 +1,5 @@
-﻿using Gtlabs.Api.ActionFilters.HeaderValidations;
+﻿using Gtlabs.Api.ActionFilters.Extensions;
+using Gtlabs.Api.ActionFilters.HeaderValidations;
 using Gtlabs.Api.AmbientData.Extensions;
 using Gtlabs.Api.ApiCall.Extensions;
 using Gtlabs.DependencyInjections.DependencyInjectons.Extensions;
@@ -10,9 +11,11 @@ public static class InitializeService
 {
     public static IServiceCollection AddBasicFeatures(this IServiceCollection services)
     {
+        services.AddHttpContextAccessor();
         services.AddAmbientData();
         services.AddApiClientCallBuilder();
         services.RegisterAllDependencies();
+        services.AddActionFilters();
         services.AddControllers(options =>
         {
             options.Filters.Add<HeaderValidationFilter>();
